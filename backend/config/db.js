baff06a2ad58416f999ba66390e9bd9ac1f29d08
Ovijit Chandra Balo <117848563+morgenstern4@users.noreply.github.com>
2025-06-28 13:59:04 +0600

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/disasterguard');
+    const conn = await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/disasterguard');
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error connecting to MongoDB: ${error.message}`);
